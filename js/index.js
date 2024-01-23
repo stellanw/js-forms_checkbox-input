e59e620e9d1e589f54d4eefbbf7636e9e1736477
Stellan Wetzig <stellanwetzig@yahoo.de>
2024-01-23 16:47:58 +0100

    add solved challenge

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
+const tosSuccess = document.querySelector('[data-js="success"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
 
 function hideTosError() {
@@ -12,13 +13,26 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideTosSuccess() {
+  tosSuccess.setAttribute("hidden", "");
+}
+
+function showTosSuccess() {
+  tosSuccess.removeAttribute("hidden");
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  if (tosCheckbox.checked) {
+    hideTosError();
+    showTosSuccess();
+  } else {
+    showTosError();
+    hideTosSuccess();
+  }
   // --^-- write your code here --^--
-
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  // alert("Form submitted");
 });
